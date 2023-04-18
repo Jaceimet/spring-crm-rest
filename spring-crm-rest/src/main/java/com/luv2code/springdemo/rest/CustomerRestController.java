7e26c60f3e3ee21306375edb174161a8a2e81a34
Jaceimet <84209692+Jaceimet@users.noreply.github.com>
2023-04-17 22:35:44 -0700

    Add get single customer option

--- spring-crm-rest/src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
+++ spring-crm-rest/src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,6 +26,18 @@ public class CustomerRestController {
 		
 		return customerService.getCustomers();
 	}
+	
+	// add mapping for GET /customer/{customerId}
+	
+	@GetMapping("/customers/{customerId}")
+	public Customer getCustomer(@PathVariable int customerId) {
+		
+		Customer theCustomer = customerService.getCustomer(customerId);
+		
+		return theCustomer;
+	}
+	
+	
 	
 	
 	
